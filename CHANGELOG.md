52b20b9ab98e9f65b81a5281beadc41a184d28e9
Renato Alves <19148962+renatonascalves@users.noreply.github.com>
2024-04-01 12:17:18 -0300

    Adding support for CSV files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.2.3] - Unpublished
+
+### Added
+
+- Add support for CSV files.
+
 ## [0.2.2] - 2024-03-12
 
 ### Changed
---
+++ tests/class-test-csv-bulk-task.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Alley\WP_Bulk_Task Tests: Test_CSV_Bulk_Task class
+ *
+ * @package alleyinteractive/wp-bulk-task
+ */
+
+declare( strict_types=1 );
+
+use Alley\WP_Bulk_Task\Bulk_Task;
+use Mantle\Testing\Concerns\Refresh_Database;
+use Mantle\Testkit\Test_Case;
+
+/**
+ * Tests for the Test_CSV_Bulk_Task class.
+ *
+ * @package alleyinteractive/wp-bulk-task
+ */
+class Test_CSV_Bulk_Task extends Test_Case {
+	use Refresh_Database;
+
+	/**
+	 * The CSV file path.
+	 *
+	 * @var string
+	 */
+	public $csv;
+
+	/**
+	 * Actions to be taken before each function in this class is run.
+	 */
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->csv = __DIR__ . '/csv-test.csv';
+	}
+
+	/**
+	 * Test the run function on a invalid CSV file.
+	 */
+	public function test_invalid_csv(): void {
+		$this->expectException( Exception::class );
+		$this->expectExceptionMessage( 'The CSV file does not exist or is not readable.' );
+
+		( new Bulk_Task( 'test_invalid_csv' ) )->run(
+			[
+				'csv' => 'invalid.csv',
+			],
+			function (): void {},
+			'csv'
+		);
+	}
+
+	/**
+	 * Tests the run function on a valid CSV file.
+	 */
+	public function test_full_run(): void {
+		( new Bulk_Task( 'test_run' ) )->run(
+			[
+				'csv' => $this->csv,
+			],
+			function ( $row ): void {
+				self::factory()->post->create( [ 'post_title' => $row[1] ] );
+			},
+			'csv'
+		);
+
+		$posts = get_posts(); // @phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.get_posts_get_posts
+		$this->assertCount( 5, $posts );
+		$this->assertEquals( 'Hello', $posts[0]->post_title );
+		$this->assertEquals( 'Hiii', $posts[1]->post_title );
+		$this->assertEquals( 'Hi', $posts[2]->post_title );
+	}
+
+	/**
+	 * Tests the resume function based on the cursor position on the CSV file.
+	 */
+	public function test_resumed_run(): void {
+		$rows = [];
+
+		$bulk_task = new Bulk_Task( 'test_resumed_run' );
+		$bulk_task->cursor->set( 4 );
+		$bulk_task->run(
+			[
+				'csv' => $this->csv,
+			],
+			function ( $row ) use ( &$rows ): void {
+				$rows[] = $row;
+				self::factory()->post->create( [ 'post_title' => $row[1] ] );
+			},
+			'csv'
+		);
+
+		$this->assertCount( 1, $rows );
+		$this->assertEquals( 05, $rows[0][0] );
+		$this->assertEquals( 'Hello', $rows[0][1] );
+
+		$posts = get_posts(); // @phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.get_posts_get_posts
+		$this->assertCount( 1, $posts );
+		$this->assertEquals( 'Hello', $posts[0]->post_title );
+	}
+}
